6ee7371a0e63c26afcb58951c382242fee60241c
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2020-08-01 15:22:41 -0300

    instaling and config cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import routes from './routes'
+import cors from 'cors';
+import routes from './routes';
 
 const PORT = 3333;
 
 const app = express();
 
+app.use(cors());
 app.use(routes);
 
 app.listen(PORT);
