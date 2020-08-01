0fffa15c380b2d5b7d99d6936e8169894f580a64
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2020-08-01 14:59:35 -0300

    adding method create

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express';
+import EmailService from '../services/EmailService';
 
 const users = [
-  { name: 'Gabriel Araujo', username: 'gabrieldeoa' }
+  { name: 'Gabriel Araujo', email: 'gabrielaraujo.dev@gmail.com' }
 ];
 
 export default {
-  async  index(req: Request, res: Response) {
+  async index(req: Request, res: Response) {
     return res.json(users);
+  },
+
+  async create(req: Request, res: Response) {
+    const emailService = new EmailService();
+    
+    const to = { name: 'Gabriel Araujo', email: 'gabrielaraujo.dev@gmail.com' };
+    const message = { subject: 'Hello World', body: 'Seja bem vindo.' };
+
+    emailService.sendMail({ to, message });
+
+    res.send();
   }
 };
